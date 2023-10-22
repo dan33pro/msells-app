553427636b37b773e7a3159ebdcf26b1ad0c208a
DagoGres <dan333pro@gmai.com>
2023-10-22 13:03:55 -0500

    View Consultar Clientes y Consultar Pedidos

--- src/containers/ContentPane.jsx
+++ src/containers/ContentPane.jsx
@@ -4,28 +4,7 @@ import { flightRouterStateSchema } from 'next/dist/server/app-render/types';
 import CardDetail from '@common/CardDetail';
 
 const ContentPane = (props) => {
-  const { filtros } = props;
-  const cardDetail = {
-    title: 'Nombre de la Ruta',
-    description: 'Esta ruta esta asiganada al Vendedor: #ID - Nombre y Apellido y al Entregador: #ID - Nombre y Apellido',
-    caracteristics: ['ID: Ruta', 'Dia/Preventa', 'Dia/Entrega'],
-    buttons: [
-      {
-        description: 'Ver pepidos',
-        classN: 'secondary',
-        handler: () => {
-          console.log('logica para ver pedidos');
-        },
-      },
-      {
-        description: 'Ver clientes',
-        classN: 'primary',
-        handler: () => {
-          console.log('logica para ver clientes');
-        },
-      },
-    ],
-  };
+  const { filtros, cardsDetail } = props;
 
   return (
     <div className={styles.ContentPane}>
@@ -42,14 +21,7 @@ const ContentPane = (props) => {
         ))}
       </div>
       <article className={styles.results}>
-        <CardDetail cardDetail={cardDetail} />
-        <CardDetail cardDetail={cardDetail} />
-        <CardDetail cardDetail={cardDetail} />
-        <CardDetail cardDetail={cardDetail} />
-        <CardDetail cardDetail={cardDetail} />
-        <CardDetail cardDetail={cardDetail} />
-        <CardDetail cardDetail={cardDetail} />
-        <CardDetail cardDetail={cardDetail} />
+        {cardsDetail.map((cardDetail) => <CardDetail cardDetail={cardDetail} />)}
       </article>
     </div>
   );
